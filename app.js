0380bcd8f9118b51d53492baf8a97f29d6515b5e
claytongentry <claytongentry2017@u.northwestern.edu>
2016-05-17 07:33:28 -0500

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var port = 3000;
-app.use(express.static(__dirname + "public/"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, function() {
   console.log("Cookin' on port " + port);
